b33305e5709420aa3a37a3353724b1c55aadc373
Ryan Geary <rtgnj42@gmail.com>
2021-09-25 18:06:08 -0400

    Add render_node test

--- src/visualize.rs
+++ src/visualize.rs
@@ -26,3 +26,15 @@ pub trait Visualize: Sized {
         )
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_render_node() {
+        let target = 8u8;
+        let target_address_string = format!("{:?}", &target as *const _);
+        assert_eq!((&target).render_node(), format!("  node [shape=plaintext]\n    \"{0}\" [label=<<TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"0\"><TR><TD PORT=\"{0}-address\"><I>{0}</I></TD><TD PORT=\"{0}-type\"><B>u8</B></TD><TD PORT=\"{0}-value\">8</TD></TR></TABLE>>];\n    ", target_address_string))
+    }
+}
