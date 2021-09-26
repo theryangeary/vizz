a12bee973e478c1a7f04faaac8ad16e6d8605442
Ryan Geary <rtgnj42@gmail.com>
2021-09-26 10:08:08 -0400

    Add TODO for automatically including referenced data

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 //! 1. Replace the crate::util::html_encode method with a proper implementation
 //! 1. Replace this list with an ez intro to the lib, which will probably look just like the
 //!    [Graph] docs
+//! 1. Automatically add referenced data to graphs
 //!
 //! Early adopters can make use of this crate by manually implementing [Visualize].
 //!
