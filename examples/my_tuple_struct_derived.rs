d3bf70f956c96e4867faefd334bdeef67057d53e
Ryan Geary <rtgnj42@gmail.com>
2021-09-26 09:59:28 -0400

    Fix derive(Visualize) for tuple structs

---
+++ examples/my_tuple_struct_derived.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+use std::fs::File;
+
+use vizz::DataDescription;
+use vizz::Graph;
+use vizz::Visualize;
+
+#[derive(Visualize)]
+struct MyStruct(u8, usize, String);
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    // create some data
+    let my_struct = MyStruct(45, 42_000_000_000, String::from("this is my tuple struct"));
+
+    // create file
+    let mut dot_file = File::create("my_struct.dot")?;
+
+    // create graph
+    Graph::new().add_node(&my_struct).write_to(&mut dot_file)?;
+
+    Ok(())
+}
