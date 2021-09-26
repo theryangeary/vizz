5d0c7ba777701bef460c5280ad5b4d449ed16e09
Ryan Geary <rtgnj42@gmail.com>
2021-09-26 10:31:41 -0400

    Improve proc_macro hygiene

---
+++ examples/my_struct_derive.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use std::fs::File;
+
+use vizz::{Graph, Visualize};
+
+#[derive(Visualize)]
+struct MyStruct<'a> {
+    my_u8: u8,
+    my_string: String,
+    my_ref: &'a String,
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    // create some data
+    let unowned_string = String::from("yabadabadoo!");
+    let my_struct = MyStruct {
+        my_u8: 42,
+        my_string: "HELLO WORLD".into(),
+        my_ref: &unowned_string,
+    };
+
+    // create file
+    let mut dot_file = File::create("my_struct.dot")?;
+
+    // create graph
+    Graph::new()
+        .add_node(&my_struct)
+        .add_node(&unowned_string)
+        .write_to(&mut dot_file)?;
+
+    Ok(())
+}
