8d6fb88a8e9e51b1ed1952670f1f7dbe25435684
Ryan Geary <rtgnj42@gmail.com>
2021-09-28 15:04:49 -0400

    Impl Visualize for Box

--- src/impls.rs
+++ src/impls.rs
@@ -3,6 +3,8 @@ use crate::data_description::DataDescription;
 use crate::data_description::Value;
 use crate::Visualize;
 
+use std::ops::Deref;
+
 impl Visualize for u8 {
     fn data(&self) -> Option<Value> {
         Some(Value::Owned(self.to_string()))
@@ -55,5 +57,17 @@ where
 {
     fn associated_data(&self) -> Option<Vec<DataDescription>> {
         Some(self.iter().map(DataDescription::from).collect())
+    }
+}
+
+impl<T> Visualize for Box<T>
+where
+    T: Visualize,
+{
+    fn data(&self) -> Option<Value> {
+        Some(Value::referenced(
+            Address::new(self.deref()),
+            DataDescription::from(self.deref()),
+        ))
     }
 }
