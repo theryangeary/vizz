f673c0814e2554f56393b92bafcd431dada3afcc
Ryan Geary <rtgnj42@gmail.com>
2021-09-28 17:44:44 -0400

    Make TABLE only if not empty

--- src/util.rs
+++ src/util.rs
@@ -4,11 +4,17 @@ pub fn html_encode(s: &str) -> String {
         .replace(">", "&gt;")
 }
 
+/// Render a table, unless there would be no rows in that table, because that is a syntax error in
+/// DOT language
 pub fn render_table(table_rows: impl Iterator<Item = String>) -> String {
-    format!(
-        r#"<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">{}</TABLE>"#,
-        table_rows.fold(String::new(), |acc, s| acc + &s)
-    )
+    let table_rows_string = table_rows.fold(String::new(), |acc, s| acc + &s);
+    match table_rows_string.len() {
+        0 => String::new(),
+        _ => format!(
+            r#"<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">{}</TABLE>"#,
+            table_rows_string
+        ),
+    }
 }
 
 pub fn type_of<T>(_: &T) -> String {
